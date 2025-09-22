4806d69e9159394e9f45ff5b79a66260a87282a1
Lucas Makinen <lucas.makinen@gmail.com>
2025-09-22 12:24:50 -0400

    docstrings + comments added

--- torch/fishnets_nn.py
+++ torch/fishnets_nn.py
@@ -1,5 +1,5 @@
 import math
-from typing import List, Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union, Callable
 
 
 import torch
@@ -27,7 +27,6 @@ from torch_geometric.nn.dense.linear import Linear
 
 
 
-
 # set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -36,7 +35,7 @@ def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 def fill_triangular_torch(x):
-    m = x.shape[0] # should be n * (n+1) / 2
+    m = x.shape[0] # should be n * (n+1) // 2
     # solve for n
     n = int(math.sqrt((0.25 + 2 * m)) - 0.5)
     idx = torch.tensor(m - (n**2 - m))
@@ -50,8 +49,17 @@ def fill_diagonal_torch(a, val):
     #a[..., torch.arange(0, a.shape[0]).to(device), torch.arange(0, a.shape[0]).to(device)] = val
     return a
 
-def construct_fisher_matrix_multiple_torch(outputs):
-    Q = torch.vmap(fill_triangular_torch)(outputs)
+def construct_fisher_matrix_multiple_torch(inputs):
+    """Batched Fisher matrix construction code
+
+
+    Args:
+        inputs (torch.tensor): upper-triangular components with which to construct Fisher Cholesky components
+                                of shape (batch, n_p*(n_p-1)//2)
+    Returns:
+        torch.tensor: batched Fisher matrices of shape (batch, n_p, n_p)
+    """
+    Q = torch.vmap(fill_triangular_torch)(inputs)
     # vmap the jnp.diag function for the batch
     _diag = torch.vmap(torch.diag)
     
@@ -68,7 +76,8 @@ def construct_fisher_matrix_multiple_torch(outputs):
 ## ADD IN AN MLP TO GET US TO THE RIGHT DIMENSIONALITY
 class MLP2(Sequential):
     def __init__(self, channels: List[int], norm: Optional[str] = None,
-                 bias: bool = True, dropout: float = 0.):
+                 bias: bool = True, dropout: float = 0., act: Callable = ReLU,
+                 do_dropout: bool = False):
         m = []
         for i in range(1, len(channels)):
             m.append(Linear(channels[i - 1], channels[i], bias=bias))
@@ -83,8 +92,9 @@ class MLP2(Sequential):
                 elif norm:
                     raise NotImplementedError(
                         f'Normalization layer "{norm}" not supported.')
-                m.append(ReLU())
-                m.append(Dropout(dropout))
+                m.append(act())
+                if do_dropout:
+                    m.append(Dropout(dropout))
 
         super().__init__(*m)
 
@@ -99,13 +109,15 @@ class FishnetsAggregation(Aggregation):
 
     Args:
         n_p (int): latent space size
+        in_size (int): input vector size (if different from n_p)
         semi_grad (bool, optional): If set to :obj:`True`, will turn off
             gradient calculation during :math:`E[X^2]` computation. Therefore,
             only semi-gradients are used during backpropagation. Useful for
             saving memory and accelerating backward computation.
             (default: :obj:`False`)
     """
-    def __init__(self, n_p: int, in_size: int = None, semi_grad: bool = False):
+    def __init__(self, n_p: int, in_size: int = None, semi_grad: bool = False, 
+                 act: Callable = ReLU):
         super().__init__()
         
         self.n_p = n_p
@@ -118,8 +130,8 @@ class FishnetsAggregation(Aggregation):
         fdim = n_p + ((n_p * (n_p + 1)) // 2)
         self.fishnets_dims = fdim
         from torch_geometric.nn import Linear
-        self.lin_1 = Linear(in_size, fdim, bias=True).to(device)
-        self.lin_2 = Linear(n_p, in_size, bias=True).to(device)
+        self.lin_1 = Linear(in_size, fdim, bias=True, act=act).to(device)
+        self.lin_2 = Linear(n_p, in_size, bias=True, act=act).to(device)
 
     def forward(self, x: Tensor, index: Optional[Tensor] = None,
                 ptr: Optional[Tensor] = None, dim_size: Optional[int] = None,
@@ -144,7 +156,7 @@ class FishnetsAggregation(Aggregation):
                              index, ptr, dim_size, dim, reduce='sum').reshape(-1, self.n_p, self.n_p)
         
         # add in the prior 
-        fisher += torch.eye(self.n_p).to(device)
+        fisher = fisher + torch.eye(self.n_p).to(device)
         
         # calculate inverse-dot product
         mle = torch.einsum('...jk,...k->...j', torch.linalg.inv(fisher), score)
