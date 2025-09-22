615f7e885a9929b05cc9724e14ce2489665872e1
Lucas Makinen <lucas.makinen@gmail.com>
2025-09-22 12:47:20 -0400

    docstrings + comments added

--- torch_code/fishnets_nn.py
+++ torch_code/fishnets_nn.py
@@ -1,5 +1,5 @@
 import math
-from typing import List, Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union, Callable
 
 
 import torch
@@ -27,7 +27,6 @@ from torch_geometric.nn.dense.linear import Linear
 
 
 
-
 # set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -51,8 +50,17 @@ def fill_diagonal_torch(a, val):
     #a[..., torch.arange(0, a.shape[0]).to(device), torch.arange(0, a.shape[0]).to(device)] = val
     return a_
 
-def construct_fisher_matrix_multiple_torch(outputs):
-    Q = torch.vmap(fill_triangular_torch)(outputs)
+def construct_fisher_matrix_multiple_torch(inputs):
+    """Batched Fisher matrix construction code
+
+
+    Args:
+        inputs (torch.tensor): upper-triangular components with which to construct Fisher Cholesky components
+                                of shape (batch, n_p*(n_p-1)//2)
+    Returns:
+        torch.tensor: batched Fisher matrices of shape (batch, n_p, n_p)
+    """
+    Q = torch.vmap(fill_triangular_torch)(inputs)
     # vmap the jnp.diag function for the batch
     _diag = torch.vmap(torch.diag)
     
@@ -69,7 +77,8 @@ def construct_fisher_matrix_multiple_torch(outputs):
 ## ADD IN AN MLP TO GET US TO THE RIGHT DIMENSIONALITY
 class MLP2(Sequential):
     def __init__(self, channels: List[int], norm: Optional[str] = None,
-                 bias: bool = True, dropout: float = 0.):
+                 bias: bool = True, dropout: float = 0., act: Callable = ReLU,
+                 do_dropout: bool = False):
         m = []
         for i in range(1, len(channels)):
             m.append(Linear(channels[i - 1], channels[i], bias=bias))
@@ -84,8 +93,9 @@ class MLP2(Sequential):
                 elif norm:
                     raise NotImplementedError(
                         f'Normalization layer "{norm}" not supported.')
-                m.append(ReLU())
-                m.append(Dropout(dropout))
+                m.append(act())
+                if do_dropout:
+                    m.append(Dropout(dropout))
 
         super().__init__(*m)
 
@@ -100,6 +110,7 @@ class FishnetsAggregation(Aggregation):
 
     Args:
         n_p (int): latent space size
+        in_size (int): input vector size (if different from n_p)
         semi_grad (bool, optional): If set to :obj:`True`, will turn off
             gradient calculation during :math:`E[X^2]` computation. Therefore,
             only semi-gradients are used during backpropagation. Useful for
@@ -107,46 +118,33 @@ class FishnetsAggregation(Aggregation):
             (default: :obj:`False`)
     """
     def __init__(self, n_p: int, in_size: int = None, semi_grad: bool = False, 
-                device=None):
+                 act: Callable = ReLU):
         super().__init__()
         
         self.n_p = n_p
         
         if in_size is None:
             in_size = n_p
-
-        if device is None:
-            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        self.device = device
         
         self.in_size = in_size
         self.semi_grad = semi_grad
         fdim = n_p + ((n_p * (n_p + 1)) // 2)
         self.fishnets_dims = fdim
         from torch_geometric.nn import Linear
-        self.lin_1 = Linear(in_size, fdim, bias=True).to(self.device)
-        self.lin_2 = Linear(n_p, in_size, bias=True).to(self.device)
+        self.lin_1 = Linear(in_size, fdim, bias=True, act=act).to(device)
+        self.lin_2 = Linear(n_p, in_size, bias=True, act=act).to(device)
 
     def forward(self, x: Tensor, index: Optional[Tensor] = None,
                 ptr: Optional[Tensor] = None, dim_size: Optional[int] = None,
                 dim: int = -2) -> Tensor:
-
-        # print("ptr", ptr)
-
-        if ptr is not None:
-            ptr = ptr.to(self.device)
-        if index is not None:
-            index.to(self.device)
-
         
         # GET X TO THE RIGHT DIMENSIONALITY
-        x = self.lin_1(x.to(self.device))
+        x = self.lin_1(x)
         
         # CONSTRUCT SCORE AND FISHER
         # the input x will be n_p + (n_p*(n_p + 1) // 2) long
-        score = x[..., :self.n_p].to(self.device)
-        fisher = x[..., self.n_p:].to(self.device)
+        score = x[..., :self.n_p]
+        fisher = x[..., self.n_p:]
         
         # reduce the score
         score = self.reduce(score, index, ptr, dim_size, dim, reduce='sum')
@@ -154,13 +152,12 @@ class FishnetsAggregation(Aggregation):
         # construct the fisher
         fisher = construct_fisher_matrix_multiple_torch(fisher)
 
-
         # sum the fishers
         fisher = self.reduce(fisher.reshape(-1, self.n_p**2), 
                              index, ptr, dim_size, dim, reduce='sum').reshape(-1, self.n_p, self.n_p)
         
         # add in the prior 
-        fisher = fisher + torch.eye(self.n_p).to(self.device)
+        fisher = fisher + torch.eye(self.n_p).to(device)
         
         # calculate inverse-dot product
         mle = torch.einsum('...jk,...k->...j', torch.linalg.inv(fisher), score)
